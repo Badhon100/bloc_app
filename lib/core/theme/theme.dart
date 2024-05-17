978e412fa00d2cf9aca8f532b7f98f975c1ff5c0
Nahiduzzaman Badhon <32125810+Badhon100@users.noreply.github.com>
2024-05-17 11:20:03 +0600

    theme and color added

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_app/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final dartkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor
+  );
+}
