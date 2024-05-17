6eed8affb64c812488b075331f34d6c62eeca823
Nahiduzzaman Badhon <32125810+Badhon100@users.noreply.github.com>
2024-05-17 15:35:45 +0600

    repository for auth created and installed supabase

---
+++ lib/core/secret/app_secret.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://syjzwsbrfbwzqhxfgnbx.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5anp3c2JyZmJ3enFoeGZnbmJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU5Mzc3MzMsImV4cCI6MjAzMTUxMzczM30.HYUdye5_tErBE3_1ukq-gj47nJgvutVlilEg-WjCCO4';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:bloc_app/core/secret/app_secret.dart';
 import 'package:bloc_app/core/theme/theme.dart';
 import 'package:bloc_app/features/auth/presentation/pages/sign_in_page.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
   runApp(const MyApp());
 }
 
